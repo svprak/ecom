556d320c15350cd02b0d88b966cfafd8efd494fa
svp <svprak@gmail.com>
2020-06-15 12:14:00 +0700

    First Commit

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello From Card</h3>
+      </div>
+    );
+  }
+}
