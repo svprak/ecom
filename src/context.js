e49727f815393820beae63164fb92194b002b3a0
svp <svprak@gmail.com>
2020-06-16 17:02:43 +0700

    working on detail page

--- src/context.js
+++ src/context.js
@@ -1,33 +1,51 @@
 import React, { Component } from 'react';
-import {storeProducts, detailProduct} from './data'
+import { storeProducts, detailProduct } from './data';
 const ProductContext = React.createContext();
 //Provider
 //Consumer
 
 class ProductProvider extends Component {
-  state={
-    products:storeProducts,
-    detailProduct:detailProduct
+  state = {
+    products: [],
+    detailProduct: detailProduct,
+  };
+
+  componentDidMount() {
+    this.setProducts();
   }
+  //Copy product array to product rather than just assign it directly where which it references to the original array.
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach((item) => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
   handleDetail = () => {
-    console.log('Hello from Details')
-  }
-  addToCart = () => {
-    console.log('Hello from add to cart')
-  }
+    console.log('Hello from Details');
+  };
+  addToCart = (id) => {
+    console.log(`Hello from add to cart: ${id}`);
+  };
   render() {
-    return <ProductContext.Provider value={
-      {
-        ...this.state,
-        handleDetail:this.handleDetail,
-        addToCart:this.addToCart
-        }
-      }>
-      {this.props.children}
-    </ProductContext.Provider>;
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart,
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
   }
 }
 
 const ProductConsumer = ProductContext.Consumer;
 
-export {ProductProvider, ProductConsumer}
+export { ProductProvider, ProductConsumer };
